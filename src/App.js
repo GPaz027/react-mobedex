0e01fd24be6c187dbd7aae212a7030040004f221
Gonzalo Paz <pazgonzalo2014@gmail.com>
2022-12-08 19:09:52 -0300

    Optimización de extracción de estado

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 	}
 
 	/* Esta función solamente se construye una sola vez en el componente (antes se creaba cada
-	vez que había un cambio en el input porque era una función anónima) */
+	vez que había un cambio en el input porque era una función anónima). Optimización 1 */
 	onSearchChange = (event) => {
 		const searchField = event.target.value.toLocaleLowerCase();
 		this.setState(() => {
@@ -39,11 +39,15 @@ class App extends Component {
 
 	render() {
 
+		/* Esto hace que sea más legible. Extracción del estado. Optimización 2 */
+		const { monsters, searchField} = this.state;
+		const { onSearchChange } = this;
+
 		// Los monsters que aparecerán serán el resultado de lo que se ponga en el input, por lo que
 		// lo que se renderizará no será el estado original, sino el resultado del filtrado.
 		// Esto de usar otro array y no el estado orignal es el principio de INMUTABILIDAD.
-		const filteredMonsters = this.state.monsters.filter((monster) => {
-			return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
 		});
 
 		return (
@@ -53,7 +57,7 @@ class App extends Component {
 					type="search"
 					placeholder="search monsters"
 					// Cada vez que se actualiza el input, el estado también.
-					onChange={this.onSearchChange}
+					onChange={onSearchChange}
 				/>
 				{filteredMonsters.map((monster) => {
 					return <h1 key={monster.id}>{monster.name}</h1>;
