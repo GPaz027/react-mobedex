a4e1975d3042b2f8800bc500e249826dabf58f14
Gonzalo Paz <pazgonzalo2014@gmail.com>
2022-12-08 18:44:20 -0300

    Original state lost

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Component, useSyncExternalStore } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// En este tipo de componentes, se corre primero el constructor, donde se inicializa el estado.
+// Luego corre el render(), que determina qué mostrar.
+// En tercer lugar, corre el componentDidMount()
+
 class App extends Component {
 	constructor() {
 		super();
@@ -26,6 +30,21 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<input
+					className="search-box"
+					type="search"
+					placeholder="search monsters"
+					onChange={(event) => {
+						console.log(event.target.value);
+						const searchString = event.target.value.toLocaleLowerCase();
+						const filteredMonsters = this.state.monsters.filter((monster) => {
+							return monster.name.toLocaleLowerCase().includes(searchString);
+						});
+						this.setState(() => {
+							return { monsters: filteredMonsters };
+						});
+					}}
+				/>
 				{this.state.monsters.map((monster) => {
 					return <h1 key={monster.id}>{monster.name}</h1>;
 				})}
